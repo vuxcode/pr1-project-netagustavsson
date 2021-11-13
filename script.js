e00f85eee07e4e7c5e195d92a1dbe3082cf6d65a
netagustavsson <91737779+netagustavsson@users.noreply.github.com>
2021-11-13 13:39:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
-const selectionButtons = document.querySelecttorAll('[data-selection]')
-const finalColumn = document.querySelecttor('[data-final-column]')
-const computerScoreSpan = document.querySelecttor('[data-computer-score]')
-const computerScoreSpan = document.querySelecttor('[data-your-score]')
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
 {
 	name: 'rock',
-	emoji: '✊'
+	emoji: '✊',
 	beats:'scissors'
-}
+},
 
 {
 	name: 'paper',
-	emoji: '✋'
+	emoji: '✋',
 	beats:'rock'
-}
+},
 
 {
 	name: 'scissors',
-	emoji: '✌️'
+	emoji: '✌️',
 	beats:'paper'
 }
-selectionButtons.forEach(selectionButtons =>) {
-	selectionButtons.addEventListener('click', e =>) {
-	const selectionName = selectionButtons.dataset.selection
-	const selection= SELECTIONS.find(selection => selection.name === selectionName)
-	makeSelection(selectionName)
+]
+selectionButtons.forEach(selectionButton => {
+	selectionButton.addEventListener('click', e => {
+	  const selectionName = selectionButton.dataset.selection
+	const selection = SELECTIONS.find(selection => selection.name === selectionName)
+	makeSelection(selection)
 
-	addSelectionResult(computerSelection, computerWinner)
-	addSelectionResult(computerSelection, yourWinner)
-
-if (yourWinner)incrementScore(yourScoreSpan)
-if (computerWinner)incrementScore(computerScoreSpan)
-
-}
-
-function incrementScore(scorSpan) { 
-	scorSpan.innerText = pareseInt(scorSpan.innerText) + 1
-}
-
-function addSelectionResult(selection, winner){
-	const div = document.createElement('div')
-	div.innerText= selection.emoji
-	div.classList.add('result-selection')
-	if (winner) div.classList.add('winner')
-	finalColumn.after(div)
-
-}
+	})
+})
 
 function makeSelection(selection) {
 	const computerSelection = randomSelection()
 	const yourWinner = isWinner(selection, computerSelection)
 	const computerWinner = isWinner(computerSelection, selection)
-	console.log(selection)
+	console.log(computerSelection)
+
+	addSelectionResult(computerSelection,computerWinner)
+	addSelectionResult(selection,yourWinner)
+
+	if (yourWinner) incrementScore(yourScoreSpan)
+	if (computerWinner) ncrementScore(computerScoreSpan)
+}
+
+function incrementScore(scorSpan) {
+	scorSpan.innerText = parseInt(scorSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+	const div = document.createElment('div')
+	div.innerText = selection.emoji
+	div.classList.add('result-selection')
+	if (winner) div.classList.add('winner')
+	finalColumn.after(div) 
 }
 
 function isWinner(selection, opponentSelection) {
-	return selection.beats === opponentSelection.name
-
+	return selectiom.beats === opponentSelection.name
 }
-
 
 function randomSelection() {
 	const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
